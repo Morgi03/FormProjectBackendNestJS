068cea27b214e63856d204315cc6e6f04e1403ae
Morgován Nimród <morgovannimrod22@gmail.com>
2023-01-12 12:29:10 +0100

    importálás javítása, session létrehozása a controllerben

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,8 +1,5 @@
-import {
-  Controller,
-  Get,
-  Render,
-} from '@nestjs/common';
+import { Controller, Get, Render, Session } from '@nestjs/common';
+import session from 'express-session';
 import { AppService } from './app.service';
 import db from './db';
 
@@ -12,7 +9,14 @@ export class AppController {
 
   @Get()
   @Render('index')
-  index() {
+  index(@Session() session: Record<string, any>) {
+    let userName = '';
+    if (session.user_id) {
+      // ...
+    } else {
+      userName = 'Guest';
+    }
+
     return { message: 'Welcome to the homepage' };
   }
 }
